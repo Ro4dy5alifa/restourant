6eb64e75a364f4963c4daf2633c1c95913d5da69
Ro4dy5alifa <119350814+Ro4dy5alifa@users.noreply.github.com>
2024-05-09 22:45:20 +0300

    Add files via upload

---
+++ Review.js
@@ -0,0 +1,53 @@
+function addReviewForm() {
+    document.getElementById("review-form").style.display = "block";
+}
+
+function submitReview() {
+    var gmailError = document.getElementById("gmailError");
+    var nameError = document.getElementById("nameError");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var review = document.getElementById("review").value;
+    var namepat = /^[a-zA-Z\s]{1,20}$/;
+    var re = /\S+@\S+\.\S+/;
+    var newReview = `
+    <div class="test-box">
+    <div class="box-top">
+    <div class="profile-img">
+    <img src="https://www.ninjaonlinedating.com/blog/wp-content/uploads/2019/08/SmileModifiedKRAK.jpg" alt="Profile Image">
+    </div>
+    <div class="name-user">
+    <strong>${name}</strong>
+    <span>${email}</span>
+    </div>
+    </div>
+    <div class="client-comment">
+    <p>"${review}"</p>
+    </div>
+    </div>
+    `;
+   
+    if (!namepat.test(name)) {
+        nameError.textContent = "invalid name.";
+        return;
+      } else {
+        nameError.textContent = "";
+      }
+    if (!re.test(email)) {
+        gmailError.textContent = "invalid email.";
+        return;
+      } else {
+        gmailError.textContent = "";
+      }
+    // Append the new review to the existing reviews container
+    var reviewContainer = document.querySelector(".test-box-container");
+    reviewContainer.innerHTML += newReview;
+
+    // Hide the review form after submission
+    document.getElementById("review-form").style.display = "none";
+
+    // Clear the input fields
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("review").value = "";
+}
